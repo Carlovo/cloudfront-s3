dbf5ca6143108572f2a29bc397c0d4b2354aa503
Carlo 'carlovo' van Overbeek <carlovo@gmail.com>
2024-08-26 21:02:27 +0200

    delete object delete markers in logging bucket after object deletion

--- resources_logging.tf
+++ resources_logging.tf
@@ -6,20 +6,30 @@ module "request_log_bucket" {
   name                  = "logging-${var.bucket_name}"
   object_ownership_type = "BucketOwnerPreferred"
 
-  lifecycle_rule = [{
-    id     = "delete-old"
-    status = "Enabled"
+  lifecycle_rule = [
+    {
+      id     = "delete-old"
+      status = "Enabled"
 
-    expiration = {
-      days = 200
-    }
+      expiration = {
+        days = 200
+      }
 
-    noncurrent_version_expiration = {
-      noncurrent_days = 10
-    }
+      noncurrent_version_expiration = {
+        noncurrent_days = 10
+      }
+    },
+    {
+      id     = "delete-rubble"
+      status = "Enabled"
 
-    abort_incomplete_multipart_upload = {
-      days_after_initiation = 1
+      abort_incomplete_multipart_upload = {
+        days_after_initiation = 1
+      }
+
+      expiration = {
+        expired_object_delete_marker = true
+      }
     }
-  }]
+  ]
 }
